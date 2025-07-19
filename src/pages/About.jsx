46b7de4c387cfc0d5064aee2c6dd34f971d8acdb
v-pawan <v-pawan@damacgroup.com>
2025-07-19 16:09:01 +0530

    minor update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
           {/* Resume Button */}
 
           <a
-            href="https://drive.google.com/file/d/1Hon6_FobueO4wBAYSQ4ZvOAEsp9Go7HZ/view?usp=sharing"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
